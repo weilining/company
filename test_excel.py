85626cd6d37d9f5ec997ac182e0923b05700a98c
Lining Wei <18277278902@163.com>
2018-05-30 17:58:29 +0800

    test change excel for python1

--- test_excel.py
+++ test_excel.py
@@ -1,26 +1,42 @@
 #coding : utf-8
-import xlwt  # 写入文件
-import xlrd  # 打开excel文件
+# import xlwt  # 写入文件
+# import xlrd  # 打开excel文件
+# from xlutils.copy import copy
+# import os.path
+# file_path='template.xls'
+# rb = xlrd.open_workbook(file_path,formatting_info=True)
+# r_sheet = rb.sheet_by_index(0)
+# wb = copy(rb)
+# ws = wb.get_sheet(0)
+# fopen = open("test.txt", 'r')
+# lines = fopen.readlines()
+# fopen.close()
+#
+# i=0
+# for line in lines:
+#     li=line.split(' ')
+#     j=0
+#     for l in li:
+#         ws.write(i,j,l)
+#         j=j+1
+#     i=i+1
+# wb.save(file_path)
+# input("Tip: press Enter , close window!")
+#coding:utf-8
+import numpy as np
+import xlwings as xw
 
-from xlutils.copy import copy
+app=xw.App(visible=True,add_book=False)
+app.display_alerts=False
+app.screen_updating=False
+filepath=r'template.xlsx'
+wb=app.books.open(filepath)
+wb.sheets['template'].range('A4').value='韦立宁'
+wb.sheets['template'].range('B22').value='0.4'
+wb.sheets['template'].range('C22').value='0.8'
+wb.sheets['template'].range('D22').value='1.2'
+wb.save()
+wb.close()
+app.quit()
+input("结束")
 
-
-
-
-fopen = open("test.txt", 'r')
-lines = fopen.readlines()
-fopen.close()
-w=xlwt.Workbook(encoding='utf-8',style_compression=0)
-ws = w.add_sheet('1') #创建一个工作表
-i=0
-for line in lines:
-    li=line.split(' ')
-    j=0
-    for l in li:
-        ws.write(i,j,l)
-        j=j+1
-    i=i+1
-# w =copy('xqtest.xls')
-# w.get_sheet(0).write(0,0,"foo")
-w.save('xqtest.xls')
-input("Tip: press Enter , close window!")
